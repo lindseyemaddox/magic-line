d43dcf11f4dc5680bebf1e2182b92a1f8030b6af
Lindsey Maddox <lindseyemaddox@gmail.com>
2016-06-02 18:03:28 -0500

    got the line vertical

--- magic-line.js
+++ magic-line.js
@@ -1,51 +1,39 @@
 
-
-$(function () {
-    $('#main > li > a').click(function(){
-        $('#main > li').removeClass('selected');
-        $(this).closest('li').addClass('selected');
-
-    });
-});
-
-document.ready(function() {
-
-    var $el, leftPos,tabSelected,
-            $tabsNav = $("#main");
-    if ($tabsNav.length > 0) {
-        var width = $('#main').width();
-        $tabsNav.css('width', width + 'px');
-
-
-        if(!tabSelected) {
-            tabSelected = $('#main li:first-child');
-            tabSelected.addClass('selected');
-        }
-        if($('#main li#line').length === 0) {
-            $tabsNav.append("<li id='line'></li>");
-        }
-
-        $("#main #line").css({
-            left: tabSelected.position().left,
-            width: tabSelected.width()
-        }).data("origLeft", $("#line").position().left);
-   
-        $("ul#main li").hover(function () {
-            $el = $(this);
-            width = $el.width();
-            $("ul#main li#line").css("width", width);
-            leftPos = $el.position().left;
-            $("ul#main li#line").css("left", leftPos);
-
-        }, function () {
-            $("ul#main li#line").css({
-                left: $(".selected").position().left,
-                width: $(".selected").width()
-            });
+    $(function(){
+        $('#main > li > a').click(function(){
+            $('#main > li').removeClass('selected');
+            $(this).closest('li').addClass('selected');
         });
 
-    }
-});
+        $(document).ready(function(){
 
+            var $el, topPos,
+                $tabsNav = $('#main'),
+                tabSelected = $('#main li:first-child'),
+                $line = $('ul#main li#line');
 
+            if($tabsNav.length > 0) {
+
+                if(!tabSelected) {
+                    tabSelected.addClass('selected');
+                }
+                
+                $line.css({
+                    top: tabSelected.position().top
+                }).data('origTop', $line.position().top);
+           
+                $('ul#main li').hover(function(){
+                    $el = $(this);
+                    topPos = $el.position().top;
+                    $line.css('top', topPos);
+
+                }, function(){
+                    $line.css({
+                        top: $('.selected').position().top,
+                    });
+                });
+
+            }
+        });
+    });
 
