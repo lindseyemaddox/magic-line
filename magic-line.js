9c0f942164777cc3ab6f4ac22d9afffb91138ef2
Lindsey Maddox <lindseyemaddox@gmail.com>
2016-06-08 11:04:40 -0500

    adjust width, indentation

--- magic-line.js
+++ magic-line.js
@@ -1,39 +1,38 @@
 
-    $(function(){
-        $('#main > li > a').click(function(){
-            $('#main > li').removeClass('selected');
-            $(this).closest('li').addClass('selected');
-        });
-
-        $(document).ready(function(){
-
-            var $el, topPos,
-                $tabsNav = $('#main'),
-                tabSelected = $('#main li:first-child'),
-                $line = $('ul#main li#line');
-
-            if($tabsNav.length > 0) {
-
-                if(!tabSelected) {
-                    tabSelected.addClass('selected');
-                }
-                
-                $line.css({
-                    top: tabSelected.position().top
-                }).data('origTop', $line.position().top);
-           
-                $('ul#main li').hover(function(){
-                    $el = $(this);
-                    topPos = $el.position().top;
-                    $line.css('top', topPos);
-
-                }, function(){
-                    $line.css({
-                        top: $('.selected').position().top,
-                    });
-                });
-
-            }
-        });
+$(function(){
+    $('#main > li > a').click(function(){
+        $('#main > li').removeClass('selected');
+        $(this).closest('li').addClass('selected');
     });
 
+    $(document).ready(function(){
+
+        var $el, topPos,
+            $tabsNav = $('#main'),
+            tabSelected = $('#main li:first-child'),
+            $line = $('ul#main li#line');
+
+        if($tabsNav.length > 0) {
+
+            if(!tabSelected) {
+                tabSelected.addClass('selected');
+            }
+            
+            $line.css({
+                top: tabSelected.position().top
+            }).data('origTop', $line.position().top);
+       
+            $('ul#main li').hover(function(){
+                $el = $(this);
+                topPos = $el.position().top;
+                $line.css('top', topPos);
+
+            }, function(){
+                $line.css({
+                    top: $('.selected').position().top,
+                });
+            });
+
+        }
+    });
+});
